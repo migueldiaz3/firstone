a9620755235b1023aa4dd91d542f6b8641b6b56d
migueldiaz3 <104711862+migueldiaz3@users.noreply.github.com>
2023-08-09 16:28:41 +0530

    secondone

---
+++ dom.js
@@ -0,0 +1,9 @@
+//console.dir(document);
+console.log(document.URL);
+console.log(document.title);
+var headerTitle=document.getElementById('header-title');
+var header=document.getElementById('main-header');
+header.style.borderBottom='solid 3px #000';
+var element=document.getElementsByClassName('title');
+element.style.color="green";
+

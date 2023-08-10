8a353112507a2d1e75f4b3134b8391b4770e7c89
migueldiaz3 <104711862+migueldiaz3@users.noreply.github.com>
2023-08-10 12:56:25 +0530

    Add files via upload

--- dom.js
+++ dom.js
@@ -4,6 +4,11 @@ console.log(document.title);
 var headerTitle=document.getElementById('header-title');
 var header=document.getElementById('main-header');
 header.style.borderBottom='solid 3px #000';
-var element=document.getElementsByClassName('title');
-element.style.color="green";
 
+
+var item=document.getElementsByClassName('list-group-item');
+console.log(item);
+item[2].style.backgroundColor='green';
+for(var i=0;i<item.length;i++){
+    item[i].style.fontWeight='bold';
+}
